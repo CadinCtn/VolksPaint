bf59a78ed5a835cacfada64df57e6ea640bbb714
KauYoshi <124740298+KauYoshi@users.noreply.github.com>
2024-05-31 12:11:35 -0300

    criação funcionario

---
+++ src/main/java/funcionario.java
@@ -0,0 +1,38 @@
+
+import java.time.LocalDate;
+
+public class funcionario {
+    private String cpf;
+    private String nome;
+    private LocalDate dataNascimento;
+
+    public funcionario(String cpf, String nome, LocalDate dataNascimento) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
